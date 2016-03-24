29b817c512c0d20eaddc5e20bf656784722bb932
AlexKiss6 <wowmail1123@yahoo.com>
2016-03-23 23:10:17 -0400

    Initial commit

---
+++ app/src/main/java/com/example/alex/codeblue/Fragments/AssistFragment.java
@@ -0,0 +1,31 @@
+package com.example.alex.codeblue.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.alex.codeblue.R;
+
+/**
+ * Created by Alex on 3/23/2016.
+ */
+public class AssistFragment extends Fragment {
+
+    public AssistFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_assist, container, false);
+    }
+}
