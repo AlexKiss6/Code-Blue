4df742e737b2da6c9e3f68b979be7940e6fd02b0
AlexKiss6 <wowmail1123@yahoo.com>
2016-03-25 19:05:19 -0400

    Added email functionality in jumpstart activity.

--- app/src/main/java/com/example/alex/codeblue/AssistInputActivities/JumpstartActivity.java
+++ app/src/main/java/com/example/alex/codeblue/AssistInputActivities/JumpstartActivity.java
@@ -2,20 +2,55 @@ package com.example.alex.codeblue.AssistInputActivities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.example.alex.codeblue.Email.SendMail;
 import com.example.alex.codeblue.R;
+public class JumpstartActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class JumpstartActivity extends AppCompatActivity {
+    //Declaring EditText
+    private EditText editTextEmail;
+    private EditText editTextSubject;
+    private EditText editTextMessage;
+
+    //Send button
+    private Button buttonSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jumpstart);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
 
+        //Initializing the views
+        editTextMessage = (EditText) findViewById(R.id.editTextMessage);
+
+        buttonSend = (Button) findViewById(R.id.buttonSend);
+
+        //Adding click listener
+        buttonSend.setOnClickListener(this);
     }
 
+
+
+
+    private void sendEmail() {
+        //Getting content for email
+        String email = "sandorferencz@yahoo.com";
+        String subject = "Sent From Android";
+        String message = editTextMessage.getText().toString().trim();
+
+        //Creating SendMail object
+        SendMail sm = new SendMail(this, email, subject, message);
+
+        //Executing sendmail to send email
+        sm.execute();
+    }
+
+    @Override
+    public void onClick(View v) {
+        sendEmail();
+    }
 }
---
+++ app/src/main/java/com/example/alex/codeblue/Email/Config.java
@@ -0,0 +1,9 @@
+package com.example.alex.codeblue.Email;
+
+/**
+ * Created by Alex on 3/25/2016.
+ */
+public class Config {
+    public static final String EMAIL = "sandorferencz66@gmail.com";
+    public static final String PASSWORD = "cleveland23";
+}
